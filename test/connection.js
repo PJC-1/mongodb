ee1a87853a56da8f19f7b17d2e8efcc5f4868ace
PJC-1 <jumpzine@gmail.com>
2017-07-25 21:13:14 -0700

    used ES6 promise library

--- test/connection.js
+++ test/connection.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
-// Connect to mongodb
-mongoose.connect('mongodb://localhost/testdb');
-// once is a listener, in this case it's listening to 'open'
-mongoose.connection.once('open', function(){
-    console.log('Connection has been made!');
-}).on('error', function(error){
-    console.log('Connection error:', error);
+// ES6 Promises
+mongoose.Promise = global.Promise;
+
+// Connect to the db before tests run
+before(function(done){
+    // Connect to mongodb
+    mongoose.connect('mongodb://localhost/testdb');
+
+    mongoose.connection.once('open', function(){
+      console.log('Connection has been made!');
+      done();
+    }).on('error', function(error){
+      console.log('Connection error:', error);
+    });
 });
