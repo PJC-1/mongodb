2b8d057aecdd214ab9d8937edd575b7a8cb410a0
PJC-1 <jumpzine@gmail.com>
2017-07-26 21:04:14 -0700

    updating a recording the db

---
+++ test/updating_test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+// 2 params: string description and function
+describe('Updating records',function(){
+
+    var char;
+
+    beforeEach(function(done){
+      char = new MarioChar({
+          name: 'Mario'
+      });
+
+      char.save().then(function(){
+          done();
+      });
+    });
+
+    it('Updates one record in the database', function(done){
+
+        MarioChar.findOneAndUpdate({name:'Mario'},{name: 'Luigi'}).then(function(){
+            MarioChar.findOne({_id: char._id}).then(function(result){
+                assert(result.name === 'Luigi');
+                done();
+            });
+        });
+
+    });
+
+});
