a557030d29c1997e3f6d1e14558605fe0ceff589
PJC-1 <jumpzine@gmail.com>
2017-07-26 21:10:55 -0700

    using the inc update operator

--- test/updating_test.js
+++ test/updating_test.js
@@ -7,7 +7,8 @@ describe('Updating records',function(){
 
     beforeEach(function(done){
       char = new MarioChar({
-          name: 'Mario'
+          name: 'Mario',
+          weight: 50
       });
 
       char.save().then(function(){
@@ -20,6 +21,17 @@ describe('Updating records',function(){
         MarioChar.findOneAndUpdate({name:'Mario'},{name: 'Luigi'}).then(function(){
             MarioChar.findOne({_id: char._id}).then(function(result){
                 assert(result.name === 'Luigi');
+                done();
+            });
+        });
+
+    });
+
+    it('increments the weight by 1', function(done){
+
+        MarioChar.update({}, { $inc: { weight: 1 } }).then(function(){
+            MarioChar.findOne({name: 'Mario'}).then(function(result){
+                assert(result.weight === 51);
                 done();
             });
         });
