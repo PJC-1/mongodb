4ae4370a5f2e7a327b8154dd733da277ef7cff1e
PJC-1 <jumpzine@gmail.com>
2017-07-27 22:43:03 -0700

    updating a record with relational data model

--- test/nesting_test.js
+++ test/nesting_test.js
@@ -5,6 +5,12 @@ const Author = require('../models/author');
 // Describe our tests
 describe('Nesting records', function(){
 
+    beforeEach(function(done){
+        mongoose.connection.collections.authors.drop(function(){
+            done();
+        });
+    });
+
     it('Creates an author with sub-documents', function(done){
         var pat = new Author({
             name: 'Patrick Rothfuss',
@@ -18,4 +24,24 @@ describe('Nesting records', function(){
         })
     });
 
+    it('Adds a book to an author', function(done){
+
+        var pat = new Author({
+            name: 'Patrick Rothfuss',
+            books: [{title: 'Name of the Wind', pages: 400}]
+        });
+
+        pat.save().then(function(){
+            Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+                // add a book to the books collection
+                record.books.push({title: "Wise Man's Fear", pages: 500});
+                record.save().then(function(){
+                    Author.findOne({name: 'Patrick Rothfuss'}).then(function(result){
+                        assert(result.books.length === 2);
+                        done();
+                    });
+                });
+            });
+        });
+    });
 });
